0f404d132fc9d1f25c61019a19324b050c997526
Alex Korobov <devasko@ro.ru>
2020-07-09 09:54:46 +0300

    Footer styles

--- js/main.js
+++ js/main.js
@@ -19,4 +19,42 @@ $( document ).ready( function()   {
             },
         }
     });
+
+
+//  Scroll down button
+
+    const scrollDown = document.querySelector( '.header__scroll a' );
+
+    scrollDown.addEventListener( 'click', function ( event ) {
+        event.preventDefault();
+
+        let target = this.getAttribute( 'href' );
+        document.querySelector('' + target ).scrollIntoView( {
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+
+    //  Scroll up button
+
+    const scrollUp = $( '.scroll-up' );
+
+
+    $( window ).on( 'scroll', () => {
+        if ( $( this ).scrollTop() > 700 ) {
+            scrollUp.fadeIn();
+        } else {
+            scrollUp.fadeOut();
+        }
+
+        if ( window.pageYOffset > 5200 ) {
+            $( '.scroll-up' ).css( { 'color': '#fff', 'border-color': '#fff'} );
+        } else {
+            $( '.scroll-up' ).css( { 'color': '#000', 'border-color': '#000'} );
+        }
+    } );
+
+    scrollUp.click( () => {
+        $( window ).scrollTo( 0, 0 );
+    } );
 });
